578d386f8c8845c6f4c945823ecf1ef97a758420
Chiel Kunkels <chiel@kunkels.me>
2016-01-30 17:22:01 +0100

    client: move history to separate file

---
+++ src/app/history.js
@@ -0,0 +1,5 @@
+'use strict';
+
+import createBrowserHistory from 'history/lib/createBrowserHistory';
+
+export default createBrowserHistory();
--- src/client/index.jsx
+++ src/client/index.jsx
@@ -1,14 +1,12 @@
 'use strict';
 
-import routes               from './routes';
-import App                  from 'app/components/App';
-import createBrowserHistory from 'history/lib/createBrowserHistory';
-import React                from 'react';
-import ReactDOM             from 'react-dom';
-import { match, Router }    from 'react-router';
-import { Provider }         from 'react-redux';
-
-const history = createBrowserHistory();
+import routes            from './routes';
+import App               from 'app/components/App';
+import history           from 'app/history';
+import React             from 'react';
+import ReactDOM          from 'react-dom';
+import { match, Router } from 'react-router';
+import { Provider }      from 'react-redux';
 
 history.listen(location => {
 	match({ routes, location: location.pathname }, (err, redirectLocation, renderProps) => {
